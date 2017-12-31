2c30905ef8917268ea9407b53196bb98c5d96187
Andre Lopes <andrevitorlopes@gmail.com>
2017-12-31 16:32:14 -0200

    listening to click event

--- src/App.js
+++ src/App.js
@@ -10,12 +10,25 @@ class App extends Component {
       { name: 'Stephanie', age: 26 }
     ]
   };
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    //DON'T DO THIS: this.state.persons[0].name = 'Maximillian';
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
