dee21666f944960bde15b9dbd24c912b2b872b1a
alopes2 <andre.lopes@exxonmobil.com>
2018-01-02 09:12:31 -0200

    adding styles

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     // When using class-based components, it`s this.props
     // like <p>My name is {this.props.name}</p>
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name}/>
