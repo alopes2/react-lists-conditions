4149ed432195e6e159c727429606284490888d8b
alopes2 <andre.lopes@exxonmobil.com>
2018-01-02 08:44:33 -0200

    adding event handler and two-way biding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,8 +5,9 @@ const person = (props) => {
     // like <p>My name is {this.props.name}</p>
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}/>
         </div>
     );
 };
